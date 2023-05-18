05b008eb6807788601a934fcd5c7fe6a86c9de43
kawabata324 <kawabataharuki48@gmail.com>
2023-05-18 22:54:11 +0900

    :sparkles: theme primaryの追加

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
         <ImageViewer placeholderImageSource={PlaceholderImage}/>
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Chose a photo"/>
+        <Button theme="primary" label="Chose a photo"/>
         <Button label="Use this photo"/>
       </View>
       <StatusBar style="auto"/>
