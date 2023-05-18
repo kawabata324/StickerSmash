935473eceee5d2fac79b82a579a0a061a6346985
kawabata324 <kawabataharuki48@gmail.com>
2023-05-18 23:02:37 +0900

    :sparkles: userが画像ライブラリから選択できるようにする関数を作成

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, View} from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
 
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
@@ -7,6 +8,20 @@ import Button from "./components/Button";
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    })
+
+    if (!result.canceled) {
+      console.log(result)
+    } else {
+      alert('You did not select any Image')
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
